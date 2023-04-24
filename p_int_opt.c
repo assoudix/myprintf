783e63bc51d56b5f7d889ff5f7061a2df4ba07ff
Omar Omar <assoudiomar@gmail.com>
2023-04-24 16:00:23 -0700

    using iteration, simplifying loops

--- p_int_opt.c
+++ p_int_opt.c
@@ -1,23 +1,38 @@
-#include <stdarg.h>
 #include "main.h"
 
 /**
- * print_number - prints a using recursion
+ * print_number - prints a number send to this function
  * @args: List of arguments
  * Return: The number of arguments printed
  */
+int print_number(va_list args)
+{
+	int n;
+	int div;
+	int len;
+	unsigned int num;
 
-int print_number(va_list args) {
-    int n = va_arg(args, int);
-    int len = 0;
-    if (n < 0) {
-        putchar('-');
-        n = -n;
-        len++;
-    }
-    if (n / 10)
-        len += print_number(n / 10);
-    putchar(n % 10 + '0');
-    len++;
-    return len;
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _putchar('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	while (num / div > 9)
+		div *= 10;
+
+	while ( div != 0 )
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
 }
